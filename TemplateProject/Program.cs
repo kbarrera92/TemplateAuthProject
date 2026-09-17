53d3692d7c0272f388a81f4d5ba6763597e35ddc
Kevin Barrera <kevin.barre92@yahoo.com>
2026-09-16 19:30:38 -0600

    feat: agregar EF Core y tablas de Identity

--- TemplateProject/Program.cs
+++ TemplateProject/Program.cs
@@ -1,10 +1,15 @@
+
+using Microsoft.EntityFrameworkCore;
+using TemplateProject.Datos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(opciones =>
+    opciones.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
